bf70d96404a21ffc68a2434cbeb973a8da1db8d9
Reece_Lu <53824755+Reece-Lu@users.noreply.github.com>
2023-11-12 15:24:13 -0800

    useState hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,29 @@
+import {useState} from 'react';
+
 const Home = () => {
 
     const title = 'Welcome to the new blog';
     const likes = 50;
     const person = {name:'yuwen',age:'10'};
     const link = "https://www.google.com/";
-    const handleClick = (name) => {
+    const handleClick = () => {
       console.log('hello '+name);
-    }
+      setName("Gerry");
+      setAge(age+100);
+    };
+
+    const [name, setName] = useState('Reece');
+    const  [age, setAge] = useState(25);
 
 
     return (
         <div>
             <h1>{ title }</h1>
             <p> Liked {likes} times</p>
-            <p> Auther:{person.name}, Age: {person.age}</p>
+            <p> Auther:{name}, Age: {age}</p>
             <p> {Math.random()*100}</p>
             <a href={link}>Google Site</a>
-            <button onClick={()=>{handleClick('yuwen')}}>Click me</button>
+            <button onClick={()=>{handleClick()}}>Click me</button>
         </div>
     );
 }
