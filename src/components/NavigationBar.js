f8a150bd333f9fdf8dd7b4dc83afa679a656e7f0
Yuwen Lu <yuwenl@oceannetworks.ca>
2024-03-26 15:13:30 -0700

    🌶️ add education info
    added my education info, but need do some improvement in the style, currently looks like so ugly

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,8 +2,6 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
 import Button from '@mui/material/Button';
 import { Link as RouterLink } from 'react-router-dom';
 
